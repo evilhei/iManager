03caf5bb5a53f727d8aba2b03cef07cc26cdcf72
Villem Heinsalu <villemheinsalu@Villems-MacBook-Pro.local>
2016-03-09 21:52:17 +0100

    fixezzzz

--- client/enterMaterialWarehouse/enterMaterialWarehouse.js
+++ client/enterMaterialWarehouse/enterMaterialWarehouse.js
@@ -10,15 +10,11 @@ Template.enterMaterialWarehouse.onCreated(function() {
 
 Template.enterMaterialWarehouse.events({
 	"change #select2": function(e) {
-		console.log($("#select2").val())
-		console.log("event works")
-		var selectedMatCode = $("#select2").val()
-		var currentMatDesc = GeneralMaterial.find({"materialCode": selectedMatCode}, {fields: {'materialDescription': 1}}).map(function(item) {
-			return {currentMatDesc: item.materialDescription}
-		})
-		console.log(currentMatDesc)
+		selectedMatCode = $("#select2").val()
+		selectedMatCode = parseInt(selectedMatCode)
+		var currentMatDesc = GeneralMaterial.findOne({"materialCode": selectedMatCode}, {fields: {'materialDescription': 1}})
+		console.log(currentMatDesc.materialDescription)
 		Session.set("currentMatDesc", selectedMatCode)	
-		console.log(Session.get('currentMatDesc'))
 
 	}
 })
