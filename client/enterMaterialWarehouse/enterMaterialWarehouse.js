0cad3bfd8405438dc2128db716c6123c90854f98
Villem Heinsalu <villemheinsalu@Villems-MacBook-Pro.local>
2016-03-09 21:01:43 +0100

    lala

--- client/enterMaterialWarehouse/enterMaterialWarehouse.js
+++ client/enterMaterialWarehouse/enterMaterialWarehouse.js
@@ -1,3 +1,6 @@
+
+
+
 Template.enterMaterialWarehouse.onCreated(function() {
 	var self = this;
 	self.autorun(function() {
@@ -6,11 +9,28 @@ Template.enterMaterialWarehouse.onCreated(function() {
 })
 
 Template.enterMaterialWarehouse.events({
-	"change": function(e) {
+	"change #select2": function(e) {
+		console.log($("#select2").val())
 		console.log("event works")
+		var selectedMatCode = $("#select2").val()
+		var currentMatDesc = GeneralMaterial.find({"materialCode": selectedMatCode}, {fields: {'materialDescription': 1}}).map(function(item) {
+			return {currentMatDesc: item.materialDescription}
+		})
+		console.log(currentMatDesc)
+		Session.set("currentMatDesc", selectedMatCode)	
+		console.log(Session.get('currentMatDesc'))
 
 	}
 })
+
+
+
+Template.enterMaterialWarehouse.onRendered(function() {
+	$("#select2").select2({
+		allowClear: true
+	});
+})
+
 
 Template.enterMaterialWarehouse.helpers({
 	matList: function() {
@@ -18,5 +38,6 @@ Template.enterMaterialWarehouse.helpers({
       	return {label: item.materialCode, value: item.materialCode}
     	})
     	return matList
+
 	}
 })
