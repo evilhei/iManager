bb42d45fc1e22bd01071c3296c8313a547365c14
Heinsalu Villem <heinsvi001@prysmian-group.com>
2016-03-10 15:47:09 +0100

    päev läbi rasket tööd

--- client/enterMaterialWarehouse/enterMaterialWarehouse.js
+++ client/enterMaterialWarehouse/enterMaterialWarehouse.js
@@ -5,26 +5,41 @@ Template.enterMaterialWarehouse.onCreated(function() {
 	var self = this;
 	self.autorun(function() {
 		self.subscribe('generalMaterialMaster');
+		self.subscribe('StorageLoc');
 	})
 })
 
 Template.enterMaterialWarehouse.events({
-	"change #select2": function(e) {
-		selectedMatCode = $("#select2").val()
+	"change #chooseMaterial": function(e) {
+		selectedMatCode = $("#chooseMaterial").val()
+		console.log(selectedMatCode + "selected")
 		selectedMatCode = parseInt(selectedMatCode)
 		var currentMatDesc = GeneralMaterial.findOne({"materialCode": selectedMatCode}, {fields: {'materialDescription': 1}})
-		console.log(currentMatDesc.materialDescription)
-		Session.set("currentMatDesc", selectedMatCode)	
-
+		currentMatDesc = currentMatDesc.materialDescription
+		console.log(currentMatDesc)
+		Session.set("currentMatDesc", currentMatDesc)	
+	},
+	"submit .enterMatWarehouse": function(e) {
+		event.preventDefault();
+		var q = e.target.quantity.value; //quantity
+		var c = $("#chooseMaterial").val() //material code
+		var s = $("#chooseStoraceLoc").val() //storace location
+		console.log(q, c, s)
+		q = parseInt(q) //make sure that q s int not string
+		Meteor.call('enterStock', q,c,s)
+		event.target.quantity.value = "";
 	}
 })
 
 
 
 Template.enterMaterialWarehouse.onRendered(function() {
-	$("#select2").select2({
+	$("#chooseMaterial").select2({
 		allowClear: true
-	});
+	}),
+	$("#chooseStoraceLoc").select2({
+		allowClear: true
+	})
 })
 
 
@@ -35,5 +50,15 @@ Template.enterMaterialWarehouse.helpers({
     	})
     	return matList
 
+	},
+	matDesc: function() {
+		matDesc = Session.get("currentMatDesc")
+		return matDesc
+	},
+	storList: function() {
+		var storList = StorageLoc.find({}, {fields: {'storageLocationName': 1}}).map(function(item) {
+			return {label: item.storageLocationName, value: item.storageLocationName}
+		})
+			return storList
 	}
 })
--- server/server.js
+++ server/server.js
@@ -15,29 +15,9 @@ Meteor.startup(function () {
 
 })
 
-Meteor.methods({
-	//vajab veel ühte meetodit, mis ei laseks üle 1 kasutaja luua.
-	'addAdminToFirstUser': function() {
-		var adminRole = 'admin';
-		var userId = Meteor.userId();
-
-		var userCount = Meteor.users.find().count();
-		if (userCount === 1) {
-			Roles.addUsersToRoles(userId, adminRole)
-		}
-	},
-
-	'getReportData': function(startDate, finishDate) {
-		/*
-		if (mingi check, mis annaks errorit?) {
-			throw new Meteor.Error("no-data-found", "No data can be found for selected period");
-		} */
-
-		var reportData = ScrapReport.find({dateEntered: { $gte: startDate, $lte: finishDate } } ).fetch();
-		return reportData;
-	}
-});
-
 Meteor.publish('generalMaterialMaster', function(){
 	return GeneralMaterial.find({})
+})
+Meteor.publish('StorageLoc', function(){
+	return StorageLoc.find({})
 })
